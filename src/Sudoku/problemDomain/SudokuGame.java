152b5a11a78aa56f9472689e38ee8712b8331352
farzane-maleki <farzane.maleki.fm@gmail.com>
2023-04-12 00:55:52 +0430

    added SudokoGame Domain

---
+++ src/Sudoku/problemDomain/SudokuGame.java
@@ -0,0 +1,22 @@
+package Sudoku.problemDomain;
+
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final int[][] gridState;
+    private final GameState gameState;
+    public static  int GRID_BOUNDERY = 9;
+    public SudokuGame(int[][] gridState, GameState gameState) {
+        this.gridState = gridState;
+        this.gameState = gameState;
+    }
+
+    public int[][] getCopyOfGridState() {
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+}
