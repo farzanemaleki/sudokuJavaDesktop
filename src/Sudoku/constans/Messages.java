436932560bfdb1abf26afbac4ef6ae4951137d78
farzane-maleki <farzane.maleki.fm@gmail.com>
2023-04-14 18:34:53 +0430

    Added Control Logic

--- src/Sudoku/constans/Messages.java
+++ src/Sudoku/constans/Messages.java
@@ -2,6 +2,6 @@ package Sudoku.constans;
 
 public class Messages {
     public static final String GAME_COMPELTE = "Congratulations, you have won! New Game? ";
-    public static final String Error = "An error has occurred ";
+    public static final String ERROR = "An error has occurred ";
 
 }
---
+++ src/Sudoku/userinterface/logic/ContorlLogic.java
@@ -0,0 +1,58 @@
+package Sudoku.userinterface.logic;
+
+import Sudoku.constans.GameState;
+import Sudoku.constans.Messages;
+import Sudoku.problemDomain.IStorage;
+import Sudoku.problemDomain.SudokuGame;
+import Sudoku.userinterface.IUserInterfaceContract;
+
+import java.io.IOException;
+
+public class ContorlLogic implements IUserInterfaceContract.EventListener {
+
+    private IStorage storage;
+    private IUserInterfaceContract.View view;
+
+    public ContorlLogic(IStorage storage, IUserInterfaceContract.View view) {
+        this.storage = storage;
+        this.view = view;
+    }
+
+    @Override
+    public void onSudokuInput(int x, int y, int input) {
+        try {
+            SudokuGame gameData = storage.getGameDate();
+            int[][] newGridState = gameData.getCopyOfGridState();
+            newGridState[x][y] = input;
+
+            gameData = new SudokuGame(
+                    GameLogic.checkForCompletion(newGridState),
+                    newGridState
+            );
+
+            storage.updateGameData(gameData);
+
+            view.updateSquare(x, y, input);
+
+            if (gameData.getGameState() == GameState.COMPLETE) {
+                view.showDialog(Messages.GAME_COMPELTE);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            view.showError(Messages.ERROR);
+        }
+    }
+
+    @Override
+    public void onDialogClick() {
+        try {
+            storage.updateGameData(
+                    GameLogic.getNewGame()
+            );
+
+            view.updateBoard(storage.getGameDate());
+        } catch (IOException e) {
+            view.showError(Messages.ERROR);
+        }
+    }
+}
