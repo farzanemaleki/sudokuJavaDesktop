f0419829e1675b3d205bf851940212658fb0030d
farzane-maleki <farzane.maleki.fm@gmail.com>
2023-04-14 20:52:54 +0430

    Added Build Logic

---
+++ src/Sudoku/buildlogic/SudokuBuildLogic.java
@@ -0,0 +1,29 @@
+package Sudoku.buildlogic;
+
+import Sudoku.computationLogic.GameLogic;
+import Sudoku.problemDomain.IStorage;
+import Sudoku.problemDomain.SudokuGame;
+import Sudoku.userinterface.IUserInterfaceContract;
+import Sudoku.userinterface.logic.ContorlLogic;
+
+import java.io.IOException;
+
+public class SudokuBuildLogic {
+    public static void build(IUserInterfaceContract.View userInterface) throws IOException {
+        SudokuGame initialState;
+        IStorage storage = new LocalStorageImpl();
+
+        try {
+
+            initialState = storage.getGameDate();
+        } catch (IOException e) {
+
+            initialState = GameLogic.getNewGame();
+            storage.updateGameData(initialState);
+        }
+
+        IUserInterfaceContract.EventListener uiLogic = new ContorlLogic(storage, userInterface);
+        userInterface.setListener(uiLogic);
+        userInterface.updateBoard(initialState);
+    }
+}
